5c445d35de5edda48e3544ac4dbd65cfaba54363
felipeumanzor <umanzor.felipe@gmail.com>
2016-01-04 16:03:11 -0300

    Widget added
    Se añade Widget, falta implementar método cuando se hace click

--- app/src/main/java/com/example/provenlogic1/googlemap_connect/Lista.java
+++ app/src/main/java/com/example/provenlogic1/googlemap_connect/Lista.java
@@ -1,12 +1,16 @@
 package com.example.provenlogic1.googlemap_connect;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 /**
  * Created by Mely on 02-Jan-16.
@@ -14,36 +18,73 @@ import android.widget.TextView;
 
 
 public class Lista extends Activity {
-
     ListView lv;
     TextView seleccionado;
     String[] locales = {"La Barricada", "La Picá de Uruguay", "Al Paso", "Cocosandwich",
-            "Chile Sandwich", "After sandwich","Tao's Sandwich & Bocatas","Kokomo delivery","La picada de Mackenna","Club sandwich",
-            "Charly dog","El Toltén","Media luna","La Bóveda","Las Muñecas del Ñielol","Zuny Tradiciones","Sangucheria Caupolican",
-            "El Horno de Mama Elba","Enkai Delivery","Kimi SuShi","Sushi Hasu Delivery","Sandwich Al Paso","Tepano´s"
+            "Chile Sandwich", "After sandwich", "Tao's Sandwich & Bocatas", "Kokomo delivery", "La picada de Mackenna", "Club sandwich",
+            "Charly dog", "El Toltén", "Media luna", "La Bóveda", "Las Muñecas del Ñielol", "Zuny Tradiciones", "Sangucheria Caupolican",
+            "El Horno de Mama Elba", "Enkai Delivery", "Kimi SuShi", "Sushi Hasu Delivery", "Sandwich Al Paso", "Tepano´s"
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lista);
-        lv=(ListView)findViewById(R.id.list);
-        seleccionado=(TextView)findViewById(R.id.seleccionado);
-        lv.setAdapter(new ArrayAdapter(this,R.layout.entrada,locales));
+        seleccionado = (TextView) findViewById(R.id.seleccionado);
+
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 
-                seleccionado.setText("Has seleccionado: "+locales[position]);
+                seleccionado.setText("Has seleccionado: " + locales[position]);
 
 
             }
         });
     }
+    class MyTask extends AsyncTask<Void,String,String>{
+        ArrayAdapter<String>adapter;
+        ProgressBar progressBar;
+        int count;
+        @Override
+        protected void onPreExecute() {
+            adapter=(ArrayAdapter<String>)lv.getAdapter();
+            progressBar=(ProgressBar)findViewById(R.id.progerss_bar);
+            progressBar.setMax(15);
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.VISIBLE);
+            count=0;
 
+        }
 
+        @Override
+        protected String doInBackground(Void... params) {
+            for(String num:locales){
+                publishProgress(num);
+                try{
+                    Thread.sleep(200);
+                }catch (InterruptedException e){
 
+                    e.printStackTrace();
+                }
+            }
+            return "todos los numero han sido agregados";
+        }
 
+        @Override
+        protected void onProgressUpdate(String... values) {
+            adapter.add(values[0]);
+            count++;
+            progressBar.setProgress(count);
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+            progressBar.setVisibility(View.GONE);
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/provenlogic1/googlemap_connect/Widget.java
@@ -0,0 +1,18 @@
+package com.example.provenlogic1.googlemap_connect;
+
+/**
+ * Created by Felipe on 04-01-2016.
+ */
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class Widget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context,
+                         AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        //Actualizar el widget
+        //...
+    }
+}
